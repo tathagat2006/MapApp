43524d86caebbc677ca08a82e14ceeb287d101db
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-06-09 12:18:46 +0530

    Initial Commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Tathagat Thapliyal on 09/06/18.
+//  Copyright © 2018 Tathagat Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
