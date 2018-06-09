866e6a5f321b64f53a43b8d4e085b00583fe1815
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-06-10 00:25:40 +0530

    Integrated API's And Basic Design Architecture

---
+++ MapApp/PlaceSuggestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaceSuggestionViewController.swift
+//  MapApp
+//
+//  Created by Tathagat Thapliyal on 09/06/18.
+//  Copyright © 2018 Tathagat Thapliyal. All rights reserved.
+//
+
+import UIKit
+
+class PlaceSuggestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
