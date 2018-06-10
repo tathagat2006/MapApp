a3b8cd018136adcfcdb82683e65ddbe20fbe80e7
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-06-10 11:58:17 +0530

    PickLabel Gesture Added

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+//USE GESTURERECOGNIZERDELEGATE....
+class ViewController: UIViewController,UIGestureRecognizerDelegate {
+    
+    //PickLabel and DropLabel Added....
+    @IBOutlet weak var PickLabel: UILabel!
+    @IBOutlet weak var DropLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        PickLabel.isUserInteractionEnabled = true
+        let PickTap = UITapGestureRecognizer.init(target: self, action: #selector(PickTapped(_sender:)))
+        PickTap.delegate = self
+        
+        //GESTURE IMPLEMENTED ON THE AREA OF PICKLABEL.. (SETTING BOUND OF THE GESTURE) PARENT KA GESTURE IS AUTO APPLICABLE TO CHILD. IF IT DOES NOT HAS IT'S OWN...
+        PickLabel.addGestureRecognizer(PickTap)
+        
+        //PickLabel.text = "Tathagat"
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //@objc to make swift wala func change to objc... coz in action above we need to pass abjc func...
+    //sender tells tht tap ke throough trigger krna hai...
+    @objc func PickTapped(_sender:UITapGestureRecognizer){
+        print("**************")
     }
 
     override func didReceiveMemoryWarning() {
