0d8371beb8b1eb5baac4911ed42a0a4223333af6
tathagat2006 <tathagat.thapliyal@gmail.com>
2018-06-10 11:01:39 +0530

    GMSservices Added And API Functioning Added

--- MapApp/AppDelegate.swift
+++ MapApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        //Add key....
+        GMSServices.provideAPIKey("AIzaSyAyDw9znr-Xq-794yoxW_34NJ2c0yCd6VQ")
+        //AIzaSyAyDw9znr-Xq-794yoxW_34NJ2c0yCd6VQ
         // Override point for customization after application launch.
         return true
     }
